8140375800f944a351d265fc5187c71e781710f7
nLeul <leulnecha@gmail.com>
2020-09-08 22:16:49 -0500

    removed comment

--- src/components/NowWhat.tsx
+++ src/components/NowWhat.tsx
@@ -23,29 +23,6 @@ export default () => {
       <CardHeader title="OK, Leul, you're all setup. Now What?" />
       <CardContent>
         <MetricsList/>
-        {/* <List>
-          <ListItem>
-            <Avatar>1</Avatar>
-            <ListItemText primary="Explore the GraphQL API" />
-          </ListItem>
-          <ListItem>
-            <Avatar>2</Avatar>
-            <ListItemText primary="Add ability to select Metrics" />
-          </ListItem>
-          <ListItem>
-            <Avatar>3</Avatar>
-            <ListItemText primary="Display the current metric data" />
-          </ListItem>
-          <ListItem>
-            <Avatar>4</Avatar>
-            <ListItemText primary="Chart historical metric data" />
-          </ListItem>
-          <ListItem>
-            <Avatar>5</Avatar>
-            <ListItemText primary="Submit Your App" />
-          </ListItem>
-        </List> */}
-
         <Typography variant="body1">
           Remember to refer to our <a href="https://react.eogresources.com/assessing">How We Assess Submissions</a>{' '}
           guidelines, as well as the <a href="https://react.eogresources.com/api">GraphQL API Documentation</a>.
